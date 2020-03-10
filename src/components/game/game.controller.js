0ea1466a70aba15f3d546b6d48aac9b8213a22af
analava <analava2002@gmail.com>
2020-03-10 18:57:33 +0330

    move function

--- src/components/game/game.controller.js
+++ src/components/game/game.controller.js
@@ -1,6 +1,6 @@
 const Game = require('./game.model');
 const playerController = require('../player/player.controller');
-const { random } = require('../../utils/random');
+const { random, choiceRange } = require('../../utils/random');
 
 exports.startGame = async(gameKind, playerName, initial_number, nextPlayer, goal) => {
     const players = [];
@@ -18,6 +18,34 @@ exports.startGame = async(gameKind, playerName, initial_number, nextPlayer, goal
     return await game.save();
 };
 
+
+exports.move = async(game_id, player_id, moveNumber) => {
+    const game = await Game.findById(game_id);
+
+    if (!game) {
+        throw new Error('Invalid Game'); // fix status
+    }
+
+    if (player_id.toString() != game.nextPlayer) {
+        throw new Error('It is not your turn!'); // fix status
+    }
+
+    const validChoiceRange = choiceRange(game.goal);
+    if (validChoiceRange.indexOf(moveNumber) == -1) {
+        throw new Error('Invalid choice number'); // fix status
+    }
+
+    let newNumber = game.currentNumber + moveNumber;
+    if (newNumber % game.goal != 0) {
+        throw new Error('Not correctly divisable by goal'); // fix status
+    } else {
+        const currentNumber = newNumber / game.goal;
+        game.currentNumber = currentNumber;
+        game.nextPlayer = game.players[(game.players.indexOf(player_id) + 1) % game.players.length];
+        await game.save();
+        return game;
+    }
+}
 
 async function createPlayersBasedOnGameKind(gameKind, playerName, nextPlayer) {
     let addPlayers;
