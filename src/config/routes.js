0214d9f260110256ce6067a8eb9d8dcb5f7ac6bc
analava <analava2002@gmail.com>
2020-03-10 20:54:51 +0330

    player route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,6 @@
-const gameRouter = require('../components/game').router;
 const express = require('express');
+const gameRouter = require('../components/game').router;
+const playerRouter = require('../components/player').router;
 
 const welcomeRouter = express.Router();
 const router = express.Router();
@@ -10,6 +11,7 @@ welcomeRouter.use('/api/v1', router);
 router.use(express.json());
 
 router.get('/sup', (req, res) => res.send('Nothing'));
+router.use('/player', playerRouter)
 router.use('/game', gameRouter);
 
 
